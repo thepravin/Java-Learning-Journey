558731dc5cfa002329dfbc8e7c7e7a94ffe6cbd3
Pravin Nalawade <thepravin28@gmail.com>
2024-12-13 23:31:49 +0530

    type casting or conversion

---
+++ _04_DataType_Conversion.java
@@ -0,0 +1,11 @@
+public class _04_DataType_Conversion {
+    public static void main(String[]args){
+            /* Implicit type Conversion */
+            int a = 12;
+            long A = a;
+
+            /* Explicit type conversion */
+            long A2 = 12;
+            int a2 = (int)A2; // type casting or conversion
+    }
+}
