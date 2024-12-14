cdb9600050aa53c323ce91b6cdb778fafd8ba4df
Pravin Nalawade <thepravin28@gmail.com>
2024-12-14 15:56:03 +0530

    methods or functions

---
+++ _10_Methods.java
@@ -0,0 +1,14 @@
+
+public class _10_Methods {
+
+    public static int sum(int a, int c) {
+        return a + c;
+    }
+
+    public static void main(String a[]) {
+        
+        int add = sum(5, 6);
+        System.out.print(add);
+    }
+
+}
