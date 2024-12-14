2703bf9f9b30927d582f84bb68e3b810f1b429d0
Pravin Nalawade <thepravin28@gmail.com>
2024-12-14 14:57:22 +0530

    Arrays

---
+++ _08_For_Each_Loop.java
@@ -0,0 +1,12 @@
+
+
+public class _08_For_Each_Loop {
+    public static void main(String a[]){
+        String names []={"pravin","amol","pruthviraj","shivam","sachit","krishna"};
+
+        for(String name : names){
+            System.out.println(name);
+        }
+
+    }
+}
