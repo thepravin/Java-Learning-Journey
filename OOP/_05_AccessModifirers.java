0341876a696d4cc6a42e0556579dfc43c0bd5cf7
Pravin Nalawade <thepravin28@gmail.com>
2024-12-18 17:39:42 +0530

    Access modifirers

---
+++ OOP/_05_AccessModifirers.java
@@ -0,0 +1,19 @@
+
+/*  class Person{
+    int age ;
+    String name;
+}
+
+class Teacher {
+    public String name;
+    private int id;
+    String degree; // default or package-private
+    
+}
+ */
+
+public class _05_AccessModifirers {
+    public static void main(String agrs[]){
+
+    }
+}
