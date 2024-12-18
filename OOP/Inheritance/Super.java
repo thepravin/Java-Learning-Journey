4a8409785691e6e9ce8c91370f744f41f0c5c38d
Pravin Nalawade <thepravin28@gmail.com>
2024-12-18 17:13:35 +0530

    Super keyword

---
+++ OOP/Inheritance/Super.java
@@ -0,0 +1,42 @@
+
+/* 
+ * - Used to access emidiate parent class variables and methods.
+ * - We can't call this and super constructor at a same time. use one at a time.
+ */
+
+class Animal {
+
+    Animal(String animal) {
+        System.out.println("Animal is " + animal);
+    }
+
+    public void display() {
+        System.out.println("I am an animal");
+    }
+}
+
+class Dog extends Animal {
+
+    Dog() {
+        super("Dog"); // pass the value in parent constructor
+    }
+
+    // overriding method
+    @Override
+    public void display() {
+        System.out.println("I am a dog");
+    }
+
+    public void printMessage() {
+        display();
+        super.display();
+    }
+}
+
+public class Super {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        dog1.printMessage();
+
+    }
+}
