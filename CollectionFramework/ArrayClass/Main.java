522e64c4f89b15b2f43b74027e286e4828407043
Pravin Nalawade <thepravin28@gmail.com>
2024-12-19 23:01:56 +0530

    map and arrays

---
+++ CollectionFramework/ArrayClass/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+
+        int idx = Arrays.binarySearch(arr, 5);
+        System.out.println(idx);
+
+        Arrays.sort(arr); // use quick sort
+
+        
+        Arrays.fill(arr,1);
+        
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+
+
+    }
+}
---
+++ CollectionFramework/CollectionClass/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer>list = new ArrayList<>();
+        list.add(34);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        list.add(9);
+        list.add(76);
+        list.add(98);
+
+        System.out.println(Collections.min(list));
+        System.out.println(Collections.max(list));
+        System.out.println(Collections.frequency(list, 12));
+
+        Collections.sort(list);
+
+    }
+}
